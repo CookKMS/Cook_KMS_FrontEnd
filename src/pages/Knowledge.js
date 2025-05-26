e97b798d2ff3f0ee621b81020c0c511652639f4b
superozing <amazingozing@gmail.com>
2025-05-26 19:44:01 +0900

    Feat/ Knowledge.js에 지식 관리 페이지로 텍스트 수정
    우와

--- src/pages/Knowledge.js
+++ src/pages/Knowledge.js
@@ -6,7 +6,7 @@ function Knowledge() {
     <>
       <Header />
       <div style={{ padding: '40px', textAlign: 'center' }}>
-        <h2>1:1 문의 페이지</h2>
+        <h2>지식 관리 페이지</h2>
         <p>문의하실 내용을 작성해주세요.</p>
       </div>
     </>
