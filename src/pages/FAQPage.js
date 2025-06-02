09836b44b30943bc3bdaa3ff32a8d4a1012c69ef
park-02 <163229907+park-02@users.noreply.github.com>
2025-06-02 16:40:26 +0900

    .Refactor ./components에 필요없는 기능 삭제
    ./components에 FAQTable,SearchBar,TabMenu 필요없는 기능 삭제
    
    FAQPage.js 필요없는 코드 삭제

--- src/pages/FAQPage.js
+++ src/pages/FAQPage.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Header from '../components/Header'; // ✅ 추가
-import TabMenu from '../components/TabMenu';
-import FAQTable from '../components/FAQTable';
-import SearchBar from '../components/SearchBar';
 import '../styles/FAQPage.css';
 
 function FAQPage() {
@@ -11,11 +8,7 @@ function FAQPage() {
       <Header /> {/* ✅ 헤더 연결 */}
       <div className="faq-page">
         <h2 className="faq-title">자주 묻는 질문</h2>
-        <TabMenu />
-        <FAQTable />
         <div className="faq-bottom">
-          <SearchBar />
-          <button className="write-button">글쓰기</button>
         </div>
       </div>
     </>
